6c8b801307871e1502522ebee96fbb8b415bf9e4
Jeff Chung <sh1001309@gmail.com>
2024-07-14 22:23:07 +0100

    feat: list todos for guest code

--- methods/guest/src/bin/merkle.rs
+++ methods/guest/src/bin/merkle.rs
@@ -4,24 +4,28 @@ use alloy_primitives::U256;
 use alloy_sol_types::SolValue;
 use risc0_zkvm::guest::env;
 
-/// input keys (ACP tree + attributes + MTR)
-/// calculate the merkle root of the ACP tree through the attributes
-/// assert_eq!(merkle_root, MTR);
-/// output with something
-
 fn main() {
+    // inputs: 
+    // 1. ACP provided by Data Processor
+    // 2. uid of the user
+    //    * ACP Tree
+    //    * ACP Merkle Root
+
+
     
-
-
-    // Read the input data for this application.
+    // TODO: 1. Read the inputs from env buffer
+    // read the inputs from env buffer
     let mut input_bytes = Vec::<u8>::new();
     env::stdin().read_to_end(&mut input_bytes).unwrap();
+
+    // TODO: 2. Parse the inputs to what we want
     // Decode and parse the input
     let number = <U256>::abi_decode(&input_bytes, true).unwrap();
 
-    // Run the computation.
-    // In this case, asserting that the provided number is even.
-    assert!(!number.bit(0), "number is not even");
+    // TODO: 3. Building the merkle tree from ACP
+    // TODO: 4. Query the MTR from db
+    // TODO: 5. Assert the MTR with the calculated MTR
+    // TODO: 6. Output with journal and MTR
 
     // Commit the journal that will be received by the application contract.
     // Journal is encoded using Solidity ABI for easy decoding in the app contract.
